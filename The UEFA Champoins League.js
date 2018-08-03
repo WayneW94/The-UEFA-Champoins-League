829540e03250eb13e5ee566579aad96c24a9e3b9
Wayne Williams <40236134+WayneW94@users.noreply.github.com>
2018-08-03 15:28:06 +0100

    Add files via upload

---
+++ The UEFA Champoins League.js
@@ -0,0 +1,86 @@
+$(document).ready(() => {
+	const $spainButton = $('.spain-button');
+	const $spainNav = $('.spain-nav');
+	
+	$spainButton.on('click', () => {
+		$spainNav.slideToggle();
+		
+	});
+	
+	
+	
+	const $italyButton = $('.italy-button');
+	const $italyNav = $('.italy-nav');
+	
+	$italyButton.on('click', () => {
+		$italyNav.slideToggle();
+	});
+	
+	
+	
+	const $englandButton = $('.england-button');
+	const $englandNav = $('.england-nav');
+	
+	$englandButton.on('click', () => {
+		$englandNav.slideToggle();
+	});
+	
+	
+	const $germanyButton = $('.germany-button');
+	const $germanyNav = $('.germany-nav');
+	
+	$germanyButton.on('click', () => {
+		$germanyNav.slideToggle();
+	});
+	
+	
+	const $netherlandsButton = $('.netherlands-button');
+	const $netherlandsNav = $('.netherlands-nav');
+	
+	$netherlandsButton.on('click', () => {
+		$netherlandsNav.slideToggle();
+	});
+	
+	
+	const $portugalButton = $('.portugal-button');
+	const $portugalNav = $('.portugal-nav');
+	
+	$portugalButton.on('click', () => {
+		$portugalNav.slideToggle();
+	});
+	
+	
+	
+	const $scotlandButton = $('.scotland-button');
+	const $scotlandNav = $('.scotland-nav');
+	
+	$scotlandButton.on('click', () => {
+		$scotlandNav.slideToggle();
+	});
+	
+	
+	const $romaniaButton = $('.romania-button');
+	const $romaniaNav = $('.romania-nav');
+	
+	$romaniaButton.on('click', () => {
+		$romaniaNav.slideToggle();
+	});
+	
+	
+	const $serbiaButton = $('.serbia-button');
+	const $serbiaNav = $('.serbia-nav');
+	
+	$serbiaButton.on('click', () => {
+		$serbiaNav.slideToggle();
+	});
+	
+
+	const $franceButton = $('.france-button');
+	const $franceNav = $('.france-nav');
+	
+	$franceButton.on('click', () => {
+		$franceNav.slideToggle();
+	});
+	
+	
+})
